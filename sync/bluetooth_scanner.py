2e8937c83bc360af992d236019375cfdbce04b9f
magdesign <mag@magdesign.ch>
2021-07-31 13:02:30 +0200

    updated bluetooth scanner

--- sync/bluetooth_scanner.py
+++ sync/bluetooth_scanner.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 
 import time
-
 from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter
 
-def callback(bt_addr, rssi, packet, additional_info):
-    print((bt_addr, rssi, packet, additional_info))
+print("scans for 10s")
 
-# scan for all TLM frames of beacons in the namespace "12345678901234678901"
+
+
+
+
+
+
+
+def callback(bt_addr, rssi, packet, additional_info):
+    #print((bt_addr, rssi, packet, additional_info))
+    print((bt_addr, additional_info))
+
+
+
+
 scanner = BeaconScanner(callback,
-    # remove the following line to see packets from all beacons
-    # below entry is only looking for my exact namespace
-#    device_filter=EddystoneFilter(namespace="4927cc84cab307e6952f"),
-    #packet_filter=EddystoneTLMFrame
+    # following line scans only for my exact namespace
+    #device_filter=EddystoneFilter(namespace="12345678901234678901"),
 )
+
+
 scanner.start()
 time.sleep(10)
 scanner.stop()
+
+
+
+
+
 
